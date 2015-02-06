20f7616eef4e1b5f1a9586c02ba320f9899abb71
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-02-06 17:56:53 +0200

    Update Table.php

--- src/Table.php
+++ src/Table.php
@@ -7,7 +7,7 @@ class Table extends \samson\cms\table\Table
 {
     public $table_tmpl = 'table/index';
 
-	public function row(& $db_row, Pager & $pager = null )
+	public function row(& $db_row, Pager & $pager = null, $module = null)
 	{
 		return m()->view($this->row_tmpl)->user($db_row)->set($pager)->output();
 	}
